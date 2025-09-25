a552f871aa840f887cec3269f44c96b51604bbc1
AakashChettay <aakash.c.me@gmail.com>
2025-09-25 21:02:14 +0530

    Done a DSA prob

---
+++ DSA/day21_binarySearch/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums, target):
+        low, high = 0, len(nums)-1
+        while low <= high:
+            #print("L: "+str(low)+" H: "+str(high))
+            mid = int((low + high) / 2)
+            #print("Mid: "+ str(mid))
+            if nums[mid] == target:
+                return mid 
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
